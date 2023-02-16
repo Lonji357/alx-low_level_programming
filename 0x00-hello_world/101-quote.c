9cf451f16243ed20822868a7e2cddbb77546882c
Lonji357 <Lonji357@gmail.com>
2023-02-16 08:51:34 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: 1 (success)
+ */
+
+int main(void)
+{
+	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}		
