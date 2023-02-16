3cb4f51f3fe56b1b6f775b244002f01ada0a9355
Lonji357 <Lonji357@gmail.com>
2023-02-16 07:43:12 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,19 +7,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like buidling a multilingual puzzle");
 		return (0);
 }
-#include <stdio.h>
-
-/**
- * main - Entry point
- * Return: Always (0)
- */
-
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
-}
-
