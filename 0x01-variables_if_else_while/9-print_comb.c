7152915b9d54d5742a8ba817cc8d4c3329580eba
Lonji357 <Lonji357@gmail.com>
2023-02-17 11:37:40 -0800

    Combination for single digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,14 @@ int main(void)
     int x;
 
     for (x = 48; x < 58; x++)
-    {
-        putchar(x);
-        if (x < 57)
+{
+		 putchar(x);
+       		 if (x < 57)
         {
-            putchar(44);
-            putchar(32);
+           	 putchar(44);
+           	 putchar(32);
         }
-    }
+}
     putchar('\n');
     return (0);
 }
