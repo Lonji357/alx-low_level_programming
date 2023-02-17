7af2d1696fdc712f918943b984bcb82b65cf26cd
Lonji357 <Lonji357@gmail.com>
2023-02-17 06:23:39 -0800

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c  = 'a';
+	char C = 'A';
+
+	while (c <= 'z')
+       	{
+	putchar(c++);
+	}
+	while (C <= 'Z')
+	{
+	putchar(C++);
+	}
+	putchar('\n');
+	return (0);
+}
+
