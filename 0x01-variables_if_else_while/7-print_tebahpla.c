f2e42e432cdf3e1d5ae78c414d639d8f3b3833ba
Lonji357 <Lonji357@gmail.com>
2023-02-17 09:59:32 -0800

    Mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c--);
+	}
+	putchar('\'n);
+	return (0);
+}
