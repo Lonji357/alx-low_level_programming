dfd1d1ce1704023302c4a0d064b78f4a084afdd7
lonji357 <lonji357@gmail.com>
2023-04-09 16:46:03 -0700

    function that reads a text file and prints it to the POSIX standard output

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -5,36 +5,32 @@
 #include "main.h"
 
 /**
- * read_textfile - reads the text file and prints it to POSIX
- * standard output.
+ * read_textfile - reads a text file and prints it to POSIX
  *
  * @filename: file name
- * @letters: number of letters it can read and print.
+ * @letters: number of letters to read and print.
  * Return: the actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd, chars_read, chars_written;
-    char *buf;
+	int fd, chars_read, chars_written;
+	char *buf;
 
-    if (filename == NULL)
-        return (0);
-    fd = open(filename, O_RDONLY);
-    if (fd == -1)
-        return (0);
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL)
-        return (0);
-    chars_read = read(fd, buf, letters);
-    if (chars_read == -1)
-        return (0);
-    for (size_t i = 0; i < chars_read; i++)
-    {
-        chars_written = write(STDOUT_FILENO, &buf[i], 1);
-        if (chars_written == -1)
-            return (0);
-    }
-    close(fd);
-    free(buf);
-    return (chars_read);
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+		return (0);
+	chars_read = read(fd, buf, letters);
+	if (chars_read == -1)
+		return (0);
+	chars_written = write(STDOUT_FILENO, buf, chars_read);
+	if (chars_written == -1)
+		return (0);
+	close(fd);
+	free(buf);
+	return (chars_written);
 }
